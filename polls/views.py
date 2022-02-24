33036950c046c2d6525c32306ed5872262369690
zsmalla <malla7454@gmail.com>
2022-02-24 17:18:13 +0900

    5 test

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.views import generic
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -50,11 +50,17 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
