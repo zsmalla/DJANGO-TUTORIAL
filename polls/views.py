7cffbbaf4d559b90cb7f0c5735aa5fbb33726685
zsmalla <malla7454@gmail.com>
2022-02-24 15:22:34 +0900

    3 View

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")  # client에게 전달
+    # 1
+    # return HttpResponse("Hello World")  # client에게 전달
+
+    # 2
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    # 3
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list
+    # }
+
+    # return HttpResponse(template.render(context, request))
+
+    # 4
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        'latest_question_list':latest_question_list
+    }
+
+    return render(request, template_name='polls/index.html', context=context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s."%question_id)
